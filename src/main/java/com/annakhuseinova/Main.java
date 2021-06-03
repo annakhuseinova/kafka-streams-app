77ca704ac18904484654035ee901edf7b02b8a70
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-04 00:34:20 +0300

    More code

--- src/main/java/com/annakhuseinova/Main.java
+++ src/main/java/com/annakhuseinova/Main.java
@@ -2,9 +2,14 @@ package com.annakhuseinova;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class Main {
@@ -22,7 +27,46 @@ public class Main {
         /**
          * 1. Create a stream from Kafka topic
          * */
-        streamsBuilder.stream("word-count-input");
+        KStream<String, String> wordCountInput = streamsBuilder.stream("word-count-input");
+        /**
+         * 2. Map values to lowercase
+         * */
+        KTable<String, Long> wordCounts =  wordCountInput.mapValues(value -> value.toLowerCase())
+         /**
+          * 3. Flatmap values split by space
+          * */
+        .flatMapValues(value -> Arrays.asList(value.split("")))
+         /**
+          * 4. Select key to apply a key (we discard the old key). selectKey() takes 2 parameters - the key and
+          * the value and returns the key of the record. Basically, it forms the key, to be short
+          * */
+        .selectKey((key, word) -> word)
+         /**
+          * 5. Group by key before aggregation
+          * */
+        .groupByKey()
+        /**
+         * 6. Count occurrences
+         * */
+        .count();
 
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        /**
+         * A Kafka client that allows for performing continuous computation on input coming from one or
+         * more input topics and sends output to zero, one, or more output topics.
+         * */
+        KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), config);
+        streams.start();
+
+        /**
+         * Printing the topology
+         * */
+        System.out.println(streams.toString());
+
+        /**
+         * Shutdown hook to correctly close the streams application
+         * */
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
